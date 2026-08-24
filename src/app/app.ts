3f1893505767369c37c74224e120b7a917e7d90f
TEJASWINIHIWARE17 <tejuhiware@gmail.com>
2026-08-24 12:11:27 +0530

    Create portfolio website

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,24 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { About } from './about/about';
+import { Skills } from './skills/skills';
+import { Projects } from './projects/projects';
+import { Experience } from './experience/experience';
+import { Contact } from './contact/contact';
+import { Navbar } from './navbar/navbar';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    About,
+    Skills,
+    Projects,
+    Experience,
+    Contact,
+    Navbar
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
