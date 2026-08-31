b0086d08f814311d90454a683414343186e24e98
TEJASWINIHIWARE17 <tejuhiware@gmail.com>
2026-08-31 17:54:53 +0530

    update

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from database.mongodb import check_database_connection
+
 
 app = FastAPI(
     title="Teju Hiware Portfolio API",
@@ -23,11 +25,22 @@ app.add_middleware(
 
 
 # ==============================
+# STARTUP
+# ==============================
+
+@app.on_event("startup")
+def startup_event():
+
+    check_database_connection()
+
+
+# ==============================
 # HOME
 # ==============================
 
 @app.get("/")
 def home():
+
     return {
         "message": "Teju Hiware Portfolio API is running"
     }
@@ -39,6 +52,7 @@ def home():
 
 @app.get("/api/health")
 def health_check():
+
     return {
         "status": "success",
         "message": "Backend is healthy"
